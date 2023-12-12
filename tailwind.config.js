f70aa95cf3669b02cff1882157deb28ceeb06c61
yamana <matsuokanana.tech@gmail.com>
2023-12-13 03:15:54 +0900

    スライダの背景画像を設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    '../todaysart/app/**/*.{html.erb, html, js}',
   ],
   plugins: [
     require("daisyui")
